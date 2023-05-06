8628f1a1900147f10c1b641f52a87b09c0952ca6
dxqt <90507395+moonarray@users.noreply.github.com>
2023-05-06 15:42:32 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd /home/container
 
-# Output Current Java Version
-java -version
-
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
